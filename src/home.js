00afd3dcb844903bb4763dbeaaf7e10d4747d36c
Adrian <adriansandu9090@gmail.com>
2024-02-28 12:37:01 +0000

    removed the pizza img from initial page load

--- src/home.js
+++ src/home.js
@@ -2,12 +2,10 @@ function initHome() {
     const home = document.createElement("div")
     home.classList.add("home")
 
-    const pizzaImg = document.createElement("img")
-    pizzaImg.src = "images/pizza-main.jpg"
-    pizzaImg.alt = "pizza"
+    
 
     home.appendChild(createParagraph("Try the best pizza in London!"))
-    home.appendChild(pizzaImg)
+    
     home.appendChild(createParagraph("Order online on Deliveroo or visit us on site"))
     return home
 }
